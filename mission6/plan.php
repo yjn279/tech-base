8bd2517f68d5c138b595c9c336fd519bd22e632d
Yuji-207 <57951904+Yuij-207@users.noreply.github.com>
2020-08-03 01:01:20 +0900

    planのdelete時に発生するモーダルを実装

--- mission6/plan.php
+++ mission6/plan.php
@@ -63,7 +63,34 @@
               <a class="btn btn-info btn-lg btn-block" href="edit.php?id=<?= $id ?>">Edit</a>
 
               <?php if($_SESSION['user'] == $name_id): ?>
-                <a class="btn btn-lg btn-block border-info text-info" href="backend/delete.php?id=<?= $id ?>">Delete</a>
+                <!-- <a class="btn btn-lg btn-block border-info text-info" href="backend/delete.php?id=<?= $id ?>">Delete</a> -->
+                
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-lg btn-block border-info text-info" data-toggle="modal" data-target="#exampleModal">
+                  Delete
+                </button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header border-0">
+                        <h5 class="modal-title" id="exampleModalLabel">本当に削除しますか？</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div class="modal-body">
+                        一度削除したプランは復元できません。
+                      </div>
+                      <div class="modal-footer border-0">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <a class="btn btn-danger" href="backend/delete.php?id=<?= $id ?>">Delete</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
               <?php endif ?>
               <!-- <p class="btn btn-info btn-lg btn-block">Add to lists</p> -->
             <?php endif ?>
