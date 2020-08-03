79d4f55279a107305b75e61855f31ea5ec66222a
Yuji-207 <57951904+Yuij-207@users.noreply.github.com>
2020-08-03 17:21:50 +0900

    headerの導入に合わせてsignupのレイアウトを変更

--- mission6/signup.php
+++ mission6/signup.php
@@ -12,33 +12,34 @@
     <?php redirect('timeline.php', !empty($_SESSION['user'])) ?>
 
     <main class="col-lg-4 mx-auto p-5">
-      <h1 class="text-info text-center mb-3">Signup</h1>
+      <h1 class="text-info text-center mb-3">新規登録</h1>
+      <form action="backend/signup.php" method="POST">
+        <h5>氏名</h5>
+        <input class="form-control mb-3" type="text" name="name" placeholder="氏名" required>
+        <h5>メールアドレス</h5>
+        <input class="form-control mb-2" type="email" name="email_1" placeholder="メールアドレス" required>
+        <input class="form-control mb-3" type="email" name="email_2" placeholder="メールアドレス（確認用）" required>
+        <h5>パスワード</h5>
+        <input class="form-control mb-2" type="password" name="password_1" placeholder="パスワード" required>
+        <input class="form-control mb-3" type="password" name="password_2" placeholder="パスワード（確認用）" required>
+        <input class="btn btn-info btn-lg btn-block mb-2" type="submit" value="登録">
+      </form>
 
-      
-
+      <!-- サインアップエラー -->
       <?php if(!empty($_GET['error'])): ?>
         <?php if($_GET['error'] == 1): ?>
-          <p class="alert alert-danger">確認用のメールアドレスが一致しません。</p>
+          <p class="alert alert-danger mt-3">確認用のメールアドレスが一致しません。</p>
         <?php elseif($_GET['error'] == 2): ?>
-          <p class="alert alert-danger">確認用のパスワードが一致しません。</p>
+          <p class="alert alert-danger mt-3">確認用のパスワードが一致しません。</p>
         <?php else: ?>
-          <p class="alert alert-danger">このメールアドレスは既に登録されています。</p>
+          <p class="alert alert-danger mt-3">
+            このメールアドレスは既に登録されています。
+            <a class="alert-link" href="login.php">こちら</a>
+            からログインしてください。
+          </p>
         <?php endif ?>
       <?php endif ?>
 
-      <form action="backend/signup.php" method="POST">
-        <h4>Name</h4>
-        <input class="form-control mb-3" type="text" name="name" placeholder="name" required>
-        <h4>Email adress</h4>
-        <input class="form-control mb-2" type="email" name="email_1" placeholder="e-mail" required>
-        <input class="form-control mb-3" type="email" name="email_2" placeholder="e-mail (confirmation)" required>
-        <h4>Password</h4>
-        <input class="form-control mb-2" type="password" name="password_1" placeholder="password" required>
-        <input class="form-control mb-3" type="password" name="password_2" placeholder="password" required>
-        <input class="btn btn-info btn-lg btn-block mb-2" type="submit" value="Signup">
-      </form>
-      <a class="btn btn-lg btn-block border-info text-info border mb-4" href="login.php">Login</a>
-      <a class="btn btn-lg btn-block border-info text-info" href="timeline.php">Timeline</a>
     </main>
     <footer>
     </footer>
